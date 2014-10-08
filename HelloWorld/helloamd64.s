a7a6bb418db98fed1f07fcca9d4d6720eea6bf09
Ketho <ngsc1@live.cn>
2014-10-08 22:37:25 +0200

    hello

--- HelloWorld/helloamd64.s
+++ HelloWorld/helloamd64.s
@@ -1,13 +1,15 @@
 # Unix/AMD64 calling convention
 .text
-hello: .asciz "Hello World!\n"
+hello:
+	.asciz "Hello World!\n"
 
 .global main
 
 main:
-		movq	$hello, %rdi	# first argument, load memory address of string
-		movq	$0, %rax		# number of vector registers used for printf is zero
-		call	printf			# call the printf subroutine
+	movq	$hello, %rdi	# load memory address of string
+	movq	$0, %rax	# no vector registers used
+	call	printf		# call the printf subroutine
 end:
-		movq	$0, %rdi
-		call	exit
+	movq	$0, %rdi	# exit code
+	call	exit
+
